fcca7f94b0a746be2313fdbd2145f0c2d8b0b491
Yvo <yvo@uni-bremen.de>
2024-11-08 12:10:59 +0100

    updated to gui config

--- modules/process_EMG/main.py
+++ modules/process_EMG/main.py
@@ -1,5 +1,8 @@
 import sys
 import argparse
+import sys
+import json
+import time
 import numpy as np
 
 # own imports; if statement for documentation
@@ -16,11 +19,6 @@ class FACSvatarMessages(FACSvatarZeroMQ):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        #self.emgToMessage = EMGtoMessage()
-
-
-
-
 
     async def sub(self):
         mu = 0  # Mittelwert
@@ -33,36 +31,32 @@ class FACSvatarMessages(FACSvatarZeroMQ):
         # Berechnung der y-Werte
         y_values = max_value * np.exp(-0.5 * ((x_values - mu) / sigma) ** 2)
 
+        # init a message dict
+        msg = dict()
+
+        # metadata in message
+        msg['frame'] = -1
+        msg['timestamp'] = time.time()
+
         for intensity in y_values:
-            msg = {"confidence": 0.98, "frame": 0, "timestamp": 0.0, "au_r":
-                {"AU01": round(intensity/100, 3),
-                 "AU02": 0.0,
-                 "AU04": 0.0,
-                 "AU05": 0.0,
-                 "AU06": 0.0,
-                 "AU07": 0.0,
-                 "AU09": 0.0,
-                 "AU10": 0.0,
-                 "AU12": 0.0,
-                 "AU14": 0.0,
-                 "AU15": 0.0,
-                 "AU17": 0.0,
-                 "AU20": 0.0,
-                 "AU23": 0.0,
-                 "AU25": 0.0,
-                 "AU26": 0.0,
-                 "AU45": 0},
-                   "gaze":
-                       {"gaze_angle_x": 0.07,
-                        "gaze_angle_y": 0.349},
-                   "pose":
-                       {"pose_Rx": 0.039,
-                        "pose_Ry": -0.032,
-                        "pose_Rz": -0.0559999999999999},
-                   "timestamp_utc": 17244184896509564}
-            await self.pub_socket.pub(msg)
-
-
+            msg['au_r'] = {"AU01": 0.0,
+                            "AU02": 0.0,
+                            "AU04": round(intensity/100, 1),
+                            "AU05": 0.0,
+                            "AU06": 0.0,
+                            "AU07": 0.0,
+                            "AU09": 0.0,
+                            "AU10": 0.0,
+                            "AU12": 0.0,
+                            "AU14": 0.0,
+                            "AU15": 0.0,
+                            "AU17": 0.0,
+                            "AU20": 0.0,
+                            "AU23": 0.0,
+                            "AU25": 0.0,
+                            "AU26": 0.0,
+                            "AU45": 0}
+            await self.pub_socket.pub(msg, "gui.face_config")
 
 if __name__ == '__main__':
     # command line arguments
@@ -73,7 +67,7 @@ if __name__ == '__main__':
                         help="IP (e.g. 192.168.x.x) of where to pub to; Default: 127.0.0.1 (local)")
     parser.add_argument("--pub_port", default="5570",
                         help="Port of where to pub to; Default: 5570")
-    parser.add_argument("--pub_key", default="openface",
+    parser.add_argument("--pub_key", default="gui.face_config",
                         help="Key for filtering message; Default: openface")
     parser.add_argument("--pub_bind", default=False,
                         help="True: socket.bind() / False: socket.connect(); Default: False")
